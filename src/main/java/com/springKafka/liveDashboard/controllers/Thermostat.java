a1497be7e96e339cf6ee3bcfffba5ff8ee098ec5
Desmond Rawls <drawls@pivotal.io>
2019-11-25 11:34:08 -0800

    Abstract the source of kafka messages with spring cloud stream.

--- src/main/java/com/springKafka/liveDashboard/controllers/Thermostat.java
+++ src/main/java/com/springKafka/liveDashboard/controllers/Thermostat.java
@@ -3,6 +3,9 @@ package com.springKafka.liveDashboard.controllers;
 import com.springKafka.liveDashboard.temperature.Reading;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,17 +13,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
+@EnableBinding(Source.class)
 public class Thermostat {
 
   @Autowired
-  private KafkaTemplate<String, Reading> kafkaTemplate;
+  private Source source;
 
   @MessageMapping("/topic/ws-temperature-source")
   public void record(int temperature) throws Exception {
     Thread.sleep(1000);
     System.out.println("recording: " + temperature);
     Reading reading = new Reading(temperature);
-    kafkaTemplate.send("temperature", reading.day.toString(), reading);
+    source.output().send(MessageBuilder.withPayload(reading).build());
     System.out.println("after sending");
   }
 
