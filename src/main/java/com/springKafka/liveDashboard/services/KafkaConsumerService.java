1c1575f312680be3dbf8c5d4ad6bb2e1d51544a7
Desmond Rawls <drawls@pivotal.io>
2019-11-25 11:01:41 -0800

    Introduce spring cloud stream

--- src/main/java/com/springKafka/liveDashboard/services/KafkaConsumerService.java
+++ src/main/java/com/springKafka/liveDashboard/services/KafkaConsumerService.java
@@ -2,20 +2,24 @@ package com.springKafka.liveDashboard.services;
 
 import com.springKafka.liveDashboard.temperature.Reading;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableBinding(Sink.class)
 public class KafkaConsumerService{
 	
 	@Autowired
 	SimpMessagingTemplate template;
 	
-	@KafkaListener(topics="${kafka.topic}")
+	@StreamListener(Sink.INPUT)
 	public void consume(@Payload Reading reading) {
 		System.out.println("reading: " + reading);
-			template.convertAndSend("/topic/ws-temperature", reading.temperature);
+		template.convertAndSend("/topic/ws-temperature", reading.temperature);
 	}
 }
